babd6bc4c1563e336dd84023f3dfbaec7301791b
iency <pwnzjake@gmail.com>
2022-07-28 21:46:28 +0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>hey kuck</h1>
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
